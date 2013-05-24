05838fac7657bcf994f9f7aff8e3f12898cc2943
Martin Jul <martin@mjul.com>
2013-05-24 14:02:33 +0200

    Added integration test for DieselCompiler

---
+++ Test/DieselCompilerIntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Diesel;
+using NUnit.Framework;
+
+namespace Test.Diesel
+{
+    [TestFixture]
+    public class DieselCompilerTest
+    {
+        [Test]
+        public void Compile_ValidModelSource_ShouldGenerateSourceCode()
+        {
+            const string modelSource = @"(namespace Test (defvaluetype EmployeeNumber int))";
+            var sourceCode = DieselCompiler.Compile(modelSource);
+            Assert.That(sourceCode, Is.Not.Empty);
+            Assert.That(sourceCode, Is.StringContaining("namespace Test"));
+        }
+    }
+}
