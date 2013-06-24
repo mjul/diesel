3f5a7c36383ea309b1ddd384f5db0655d43de429
Martin Jul <martin@mjul.com>
2013-06-24 14:15:36 +0200

    Visitor pattern for System Type mapping

--- Diesel/CodeGeneration/SystemTypeMapper.cs
+++ Diesel/CodeGeneration/SystemTypeMapper.cs
@@ -4,53 +4,78 @@ using Diesel.Parsing.CSharp;
 
 namespace Diesel.CodeGeneration
 {
-    public class SystemTypeMapper
+    public class SystemTypeMapper 
     {
-         public System.Type SystemTypeFor(TypeNode node)
-         {
-             var instance = new SystemTypeMapper();
-             return instance.MapToSystemType((dynamic)node);
-         }
-
-        private Type MapToSystemType(SimpleType node)
+        public static System.Type SystemTypeFor(TypeNode node)
         {
-            return node.Type;
+            var visitor = Visit(node);
+            if (!visitor.FoundSystemType) throw new ArgumentOutOfRangeException("node", node, "No System type found for TypeNode.");
+            return visitor.Result;
         }
 
-        private Type MapToSystemType(StringReferenceType node)
+        public static bool IsSystemType(TypeNameTypeNode typeNameTypeNode)
         {
-            return typeof (string);
+            return Visit(typeNameTypeNode).FoundSystemType;
         }
 
-        private Type MapToSystemType(NullableType node)
+        private static SystemTypeMapperVisitor Visit(TypeNode node)
         {
-            Type underlying = MapToSystemType((dynamic) node.Underlying);
-            return Type.GetType(String.Format("System.Nullable`1[{0}]", underlying.FullName));
+            var visitor = new SystemTypeMapperVisitor();
+            node.Accept(visitor);
+            return visitor;
         }
 
-
-        private Type MapToSystemType(ArrayType node)
+        private class SystemTypeMapperVisitor : ITypeNodeVisitor
         {
-            Type underlying = MapToSystemType((dynamic)node.Type);
-            var rankSpecifiers = (from rank in node.RankSpecifiers.Ranks.Reverse()
-                                  from rankSpec in String.Format("[{0}]",
-                                                                 String.Join("", Enumerable.Repeat(',', rank.Dimensions - 1)))
-                                  select rankSpec);
-            return Type.GetType(String.Format("{0}{1}", 
-                                    underlying.FullName, 
-                                    String.Concat(rankSpecifiers)));
-        }
+            public Type Result { get; private set; }
+            public bool FoundSystemType
+            {
+                get { return (null != Result); }
+            }
 
+            public void Visit(StringReferenceType typeNode)
+            {
+                Result = typeof (String);
+            }
 
-        // TODO: we should probably not support these, rather fix the code gen to emit the name directly
-        private Type MapToSystemType(TypeNameTypeNode node)
-        {
-            var knownType = Type.GetType(node.TypeName.Name, false);
-            if (knownType != null) return knownType;
-            var asQualifiedSystemType = String.Format("System.{0}", node.TypeName.Name);
-            var knownSystemType = Type.GetType(asQualifiedSystemType, false);
-            if (knownSystemType != null) return knownSystemType;
-            throw new ArgumentOutOfRangeException("node", node.TypeName.Name, "Unknown type name.");
+            public void Visit(ArrayType typeNode)
+            {
+                Type underlying = SystemTypeFor(typeNode.Type);
+                var rankSpecifiers = (from rank in typeNode.RankSpecifiers.Ranks.Reverse()
+                                      from rankSpec in String.Format("[{0}]",
+                                                                     String.Join("", Enumerable.Repeat(',', rank.Dimensions - 1)))
+                                      select rankSpec);
+                Result = Type.GetType(String.Format("{0}{1}",
+                                                    underlying.FullName,
+                                                    String.Concat(rankSpecifiers)));
+            }
+
+            public void Visit(SimpleType typeNode)
+            {
+                Result = typeNode.Type;
+            }
+
+            public void Visit(NullableType typeNode)
+            {
+                Type underlying = SystemTypeFor(typeNode.Underlying);
+                Result = Type.GetType(String.Format("System.Nullable`1[{0}]", underlying.FullName));
+            }
+
+            public void Visit(TypeNameTypeNode typeNode)
+            {
+                var knownSystemType = TryGetSystemTypeFor(typeNode);
+                Result = knownSystemType;
+            }
+
+            private Type TryGetSystemTypeFor(TypeNameTypeNode node)
+            {
+                var knownType = Type.GetType(node.TypeName.Name, false);
+                if (knownType != null) return knownType;
+                var asQualifiedSystemType = String.Format("System.{0}", node.TypeName.Name);
+                var knownSystemType = Type.GetType(asQualifiedSystemType, false);
+                return knownSystemType;
+            }
+
         }
     }
 }
--- Test/CodeGeneration/SystemTypeMapperTest.cs
+++ Test/CodeGeneration/SystemTypeMapperTest.cs
@@ -11,18 +11,16 @@ namespace Test.Diesel.CodeGeneration
         [Test]
         public void SystemTypeFor_ArrayTypeOfSimpleTypeUnidimensional_ShouldMapToType()
         {
-            var sut = new SystemTypeMapper();
-            var actual = sut.SystemTypeFor(new ArrayType(new SimpleType(typeof (double)),
-                                                         new RankSpecifiers(new[] {new RankSpecifier(1)})));
+            var actual = SystemTypeMapper.SystemTypeFor(new ArrayType(new SimpleType(typeof (double)),
+                                                                      new RankSpecifiers(new[] {new RankSpecifier(1)})));
             Assert.That(actual, Is.EqualTo(typeof (double[])));
         }
 
         [Test]
         public void SystemTypeFor_ArrayTypeOfSimpleTypeRankTwo_ShouldMapToType()
         {
-            var sut = new SystemTypeMapper();
-            var actual = sut.SystemTypeFor(new ArrayType(new SimpleType(typeof (double)),
-                                                         new RankSpecifiers(new[] {new RankSpecifier(2)})));
+            var actual = SystemTypeMapper.SystemTypeFor(new ArrayType(new SimpleType(typeof (double)),
+                                                                      new RankSpecifiers(new[] {new RankSpecifier(2)})));
             Assert.That(actual, Is.EqualTo(typeof (double[,])));
         }
 
@@ -30,13 +28,12 @@ namespace Test.Diesel.CodeGeneration
         [Test]
         public void SystemTypeFor_ArrayTypeOfSimpleTypeRankOneTwo_ShouldMapToType()
         {
-            var sut = new SystemTypeMapper();
-            var actual = sut.SystemTypeFor(new ArrayType(new SimpleType(typeof(float)),
-                                                         new RankSpecifiers(new[]
-                                                             {
-                                                                 new RankSpecifier(1),
-                                                                 new RankSpecifier(2)
-                                                             })));
+            var actual = SystemTypeMapper.SystemTypeFor(new ArrayType(new SimpleType(typeof (float)),
+                                                                      new RankSpecifiers(new[]
+                                                                          {
+                                                                              new RankSpecifier(1),
+                                                                              new RankSpecifier(2)
+                                                                          })));
             Assert.That(actual, Is.EqualTo(typeof(float[][,])));
         }
 
@@ -44,14 +41,13 @@ namespace Test.Diesel.CodeGeneration
         [Test]
         public void SystemTypeFor_ArrayTypeOfSimpleTypeRankOneTwoThree_ShouldMapToType()
         {
-            var sut = new SystemTypeMapper();
-            var actual = sut.SystemTypeFor(new ArrayType(new SimpleType(typeof (float)),
-                                                         new RankSpecifiers(new[]
-                                                             {
-                                                                 new RankSpecifier(1),
-                                                                 new RankSpecifier(2), 
-                                                                 new RankSpecifier(3)
-                                                             })));
+            var actual = SystemTypeMapper.SystemTypeFor(new ArrayType(new SimpleType(typeof (float)),
+                                                                      new RankSpecifiers(new[]
+                                                                          {
+                                                                              new RankSpecifier(1),
+                                                                              new RankSpecifier(2),
+                                                                              new RankSpecifier(3)
+                                                                          })));
             Assert.That(actual, Is.EqualTo(typeof(float[][,][,,])));
         }
 
@@ -59,42 +55,50 @@ namespace Test.Diesel.CodeGeneration
         [Test]
         public void SystemTypeFor_NullableTypeOfSimpleType_ShouldMapToType()
         {
-            var sut = new SystemTypeMapper();
-            var actual = sut.SystemTypeFor(new NullableType(new SimpleType(typeof (decimal))));
+            var actual = SystemTypeMapper.SystemTypeFor(new NullableType(new SimpleType(typeof (decimal))));
             Assert.That(actual, Is.EqualTo(typeof (decimal?)));
         }
 
         [Test]
         public void SystemTypeFor_SimpleType_ShouldMapToType()
         {
-            var sut = new SystemTypeMapper();
-            var actual = sut.SystemTypeFor(new SimpleType(typeof(float)));
+            var actual = SystemTypeMapper.SystemTypeFor(new SimpleType(typeof(float)));
             Assert.That(actual, Is.EqualTo(typeof(float)));
         }
 
         [Test]
         public void SystemTypeFor_ReferenceTypeString_ShouldMapToType()
         {
-            var sut = new SystemTypeMapper();
-            var actual = sut.SystemTypeFor(new StringReferenceType());
+            var actual = SystemTypeMapper.SystemTypeFor(new StringReferenceType());
             Assert.That(actual, Is.EqualTo(typeof(string)));
         }
 
         [Test]
         public void SystemTypeFor_NamedTypeFromSystemNamespaceDateTime_ShouldMapToType()
         {
-            var sut = new SystemTypeMapper();
-            var actual = sut.SystemTypeFor(new TypeNameTypeNode(new TypeName("DateTime")));
+            var actual = SystemTypeMapper.SystemTypeFor(new TypeNameTypeNode(new TypeName("DateTime")));
             Assert.That(actual, Is.EqualTo(typeof(DateTime)));
         }
 
         [Test]
         public void SystemTypeFor_NamedTypeFromSystemNamespaceGuid_ShouldMapToType()
         {
-            var sut = new SystemTypeMapper();
-            var actual = sut.SystemTypeFor(new TypeNameTypeNode(new TypeName("Guid")));
+            var actual = SystemTypeMapper.SystemTypeFor(new TypeNameTypeNode(new TypeName("Guid")));
             Assert.That(actual, Is.EqualTo(typeof(Guid)));
         }
 
+        [Test]
+        public void IsSystemType_ForTypeNameTypeNodeKnownType_ShouldBeTrue()
+        {
+            var actual = SystemTypeMapper.IsSystemType(new TypeNameTypeNode(new TypeName("Guid")));
+            Assert.That(actual, Is.True);
+        }
+
+        [Test]
+        public void IsSystemType_NonSystemTypes_ShouldBeTrue()
+        {
+            var actual = SystemTypeMapper.IsSystemType(new TypeNameTypeNode(new TypeName("Foo.Bar.NotSystemType")));
+            Assert.That(actual, Is.False);
+        }
     }
 }
