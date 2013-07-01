c144ad3521c26adfe891098a1a2a0d9a0b1e7567
Martin Jul <martin@mjul.com>
2013-07-01 11:08:16 +0200

    Added documentation

--- Diesel/Transformations/ApplyDefaults.cs
+++ Diesel/Transformations/ApplyDefaults.cs
@@ -5,11 +5,18 @@ using Diesel.Parsing.CSharp;
 
 namespace Diesel.Transformations
 {
+    /// <summary>
+    /// Apply defaults to the model specified in the AST.
+    /// </summary>
     public class ApplyDefaults : ModelTransformation
     {
         private static readonly SimpleType DefaultPropertyType = new SimpleType(typeof(Int32));
         private const string DefaultPropertyName = "Value";
 
+        /// <summary>
+        /// Add the default type and property name to 
+        /// <see cref="ValueTypeDeclaration"/> if they have not been specified.
+        /// </summary>
         public override ValueTypeDeclaration Transform(ValueTypeDeclaration valueTypeDeclaration)
         {
             var result = base.Transform(valueTypeDeclaration);
